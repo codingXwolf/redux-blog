c5d2587dc26e7ba1e7f7a9df463a51813e044e81
Jaime Ortiz <jxme88@gmail.com>
2018-12-27 16:34:55 -0800

    test commit 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,23 +13,37 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 //     };
 // };
 
-export const fetchPosts = () => {
-    //unlimited power throuout the redux application.
-    //You can pass actions to the dispatch function. Those actions will be sent through all of the apps
-    // different middlewares and eventually forwarding it off to apps reducers.
-    //getState function can be called on a redux store and will return all of the data or all of the state inside.
-    
-    //Main take away of redux thunk is when the action gets passed to thunk it will check if its a function or an object.
-    // - If it a function it will call the dispatch and getState funcions, invoke function with dispatch,
-    // - wait for our request to finish, once request is complete it will dispatch function manually,
-    // - You will have a new action which will be passed again to redux thunk and will check if its an object or function.
-    // - this time it will be an object and pass the object to the reducers. Thats it!!
-    return function(dispatch, getState){
-        const response = jsonPlaceholder.get('/posts')
-    }
+//unlimited power throuout the redux application.
+//You can pass actions to the dispatch function. Those actions will be sent through all of the apps
+// different middlewares and eventually forwarding it off to apps reducers.
+//getState function can be called on a redux store and will return all of the data or all of the state inside.
 
-    return {
-        type: 'FETCH_POST',
-        payload: response
-    };
+//Main take away of redux thunk is when the action gets passed to thunk it will check if its a function or an object.
+// - If it a function it will call the dispatch and getState funcions, invoke function with dispatch,
+// - wait for our request to finish, once request is complete it will dispatch function manually,
+// - You will have a new action which will be passed again to redux thunk and will check if its an object or function.
+// - this time it will be an object and pass the object to the reducers. Thats it!!
+
+//original version
+// export const fetchPosts = () => {
+//     return async function(dispatch, getState){
+//         const response = await jsonPlaceholder.get('/posts')
+
+//         dispatch({ type: 'FETCH_POSTS', payload: response }) //dispatch passes in an object
+//     };
+// };
+
+//refactored version/less syntax, probaly more commonly seen.
+export const fetchPosts = () =>  async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response }); //dispatch passes in an object
 };
+
+
+//Totally fine to do.
+// export const selectPost = () => {
+//     return {
+//         type:'SELECT_POST'
+//     }
+// };
---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,4 @@
+export default () => {
+// if you dont return a value (primitive values except undefined) you will get an error because of the rules of reducers.
+// Not allowed to return UNDEFINED!!
+};
