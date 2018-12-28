6dae97535672d4c726e7d2bf874f3b0c50035ac6
Jaime Ortiz <jxme88@gmail.com>
2018-12-28 13:20:59 -0800

    saving before merging

--- src/reducers/postsReducer.js
+++
@@ -1,4 +0,0 @@
-export default () => {
-// if you dont return a value (primitive values except undefined) you will get an error because of the rules of reducers.
-// Not allowed to return UNDEFINED!!
-};
